06c71d740f2ec099e76223a95e4454a6d02d6cbb
Destom <d3stom@gmail.com>
2022-09-05 20:19:13 +0100

    added the knight

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ import classes
 
 ### Game options ###
 board_size = 5
-creature_count = 3
+creature_count = 1
 
+#initalizing game
+alive_count = 0
 my_board= classes.Board(board_size)
 barian = classes.Barian(my_board)
 my_board.put_piece(barian)
-
 creature_list = []
 for creature in range(1,creature_count +1):
     creature_list.append(classes.Creature(my_board,creature,'king'))
+    alive_count +=1
+
 
 print('welcome to Chessbarian')
-while barian.alive:
+while barian.alive and alive_count > 0:
     print('Board as it stands')
     my_board.print_board()
     for creature in creature_list:   
         creature.printCreatureState()
     barian.barian_move(my_board,creature_list)
+    alive_count = 0
     for creature in creature_list:
         if creature.alive:
+            alive_count += 1
             creature.moveCreature(my_board, barian)
-print('The barbarian is dead long live the barbarian')
+
+if barian.alive == False:
+    print('The barbarian is dead long live the barbarian')
+elif alive_count == 0:
+    print('you killed all the creatues, yay')
+else:
+    print('I have no idea why this game ended, maybe you should get a real dev to make your games')
